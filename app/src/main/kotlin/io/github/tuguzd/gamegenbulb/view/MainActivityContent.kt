940fa31200f8a0df2ef4b6203cdb0508b8996911
tuguzD <0damir.1tugushev@gmail.com>
2023-06-01 12:38:41 +0300

    Remove multiple navigation back stack as it's not working properly

--- app/src/main/kotlin/io/github/tuguzd/gamegenbulb/view/MainActivityContent.kt
+++ app/src/main/kotlin/io/github/tuguzd/gamegenbulb/view/MainActivityContent.kt
@@ -2,6 +2,7 @@ package io.github.tuguzd.gamegenbulb.view
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +11,6 @@ import com.ramcosta.composedestinations.rememberNavHostEngine
 import io.github.tuguzd.gamegenbulb.view.screen.NavGraphs
 import io.github.tuguzd.gamegenbulb.view.theme.AppTheme
 import io.github.tuguzd.gamegenbulb.view.util.AppBottomBar
-import io.github.tuguzd.gamegenbulb.view.util.AppScaffold
 import io.github.tuguzd.gamegenbulb.view.util.AppTopBar
 
 @Composable
@@ -25,17 +25,12 @@ fun AppScreen() {
     val engine = rememberNavHostEngine()
     val navController = engine.rememberNavController()
 
-    val startRoute = NavGraphs.root.startRoute
-
-    AppScaffold(
-        startRoute = startRoute,
-        navController = navController,
+    Scaffold(
         topBar = { AppTopBar() },
         bottomBar = { AppBottomBar(navController) },
     ) { padding ->
         DestinationsNavHost(
             engine = engine,
-            startRoute = startRoute,
             navController = navController,
             navGraph = NavGraphs.root,
             modifier = Modifier.padding(padding),
--- app/src/main/kotlin/io/github/tuguzd/gamegenbulb/view/util/AppBottomBar.kt
+++ app/src/main/kotlin/io/github/tuguzd/gamegenbulb/view/util/AppBottomBar.kt
@@ -7,61 +7,49 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavHostController
+import androidx.navigation.NavController
+import androidx.navigation.NavOptionsBuilder
 import com.ramcosta.composedestinations.navigation.navigate
-import com.ramcosta.composedestinations.navigation.popBackStack
-import com.ramcosta.composedestinations.navigation.popUpTo
-import com.ramcosta.composedestinations.utils.isRouteOnBackStack
 import io.github.tuguzd.gamegenbulb.view.screen.AppDestination
 import io.github.tuguzd.gamegenbulb.view.screen.NavGraphs
+import io.github.tuguzd.gamegenbulb.view.screen.appCurrentDestinationAsState
+import io.github.tuguzd.gamegenbulb.view.screen.destinations.Destination
+import io.github.tuguzd.gamegenbulb.view.screen.startAppDestination
 
 @Composable
 fun AppBottomBar(
-    navController: NavHostController,
-) = NavigationBar {
-    AppDestination.values().forEach { destination ->
-        AppBottomBarItem(destination, navController)
+    navController: NavController,
+) {
+    val currentDestination: Destination =
+        navController.appCurrentDestinationAsState().value
+            ?: NavGraphs.root.startAppDestination
+
+    NavigationBar {
+        AppDestination.values().forEach { destination ->
+            AppBottomBarItem(destination, currentDestination, navController)
+        }
     }
 }
 
 @Composable
 fun RowScope.AppBottomBarItem(
     destination: AppDestination,
-    navController: NavHostController,
+    currentDestination: Destination,
+    navController: NavController,
 ) {
-    val isCurrDestOnBackStack =
-        navController.isRouteOnBackStack(destination.direction)
     val label = stringResource(destination.label)
 
     NavigationBarItem(
-        selected = isCurrDestOnBackStack,
-        onClick = { onItemClick(destination, navController, isCurrDestOnBackStack) },
+        selected = currentDestination == destination.direction,
+        onClick = {
+            navController.navigate(
+                direction = destination.direction,
+                navOptionsBuilder = fun NavOptionsBuilder.() {
+                    launchSingleTop = true
+                },
+            )
+        },
         icon = { Icon(destination.icon, label) },
         label = { Text(label) },
     )
-}
-
-private fun onItemClick(
-    destination: AppDestination,
-    navController: NavHostController,
-    isCurrDestOnBackStack: Boolean,
-) {
-    if (isCurrDestOnBackStack) {
-        // When we click again on a bottom bar item and
-        // it was already selected we want to pop the back stack
-        // until the initial destination of this bottom bar item
-        navController.popBackStack(destination.direction, false)
-        return
-    }
-    navController.navigate(destination.direction) {
-        // Pop up to the root of the graph to
-        // avoid building up a large stack of destinations
-        // on the back stack as users select items
-        popUpTo(NavGraphs.root) { saveState = true }
-        // Avoid multiple copies of the same destination
-        // when re-selecting the same item
-        launchSingleTop = true
-        // Restore state when re-selecting a previously selected item
-        restoreState = true
-    }
 }
